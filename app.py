dafe2190ed8ce1e5abebba537fa2352f7fe33df1
Mona <itsaysusernamenotallowed@gmail.com>
2024-06-10 17:43:37 -0400

    make new structure with 3 pages

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 """
-flask app
+flask app/ui
 """
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# display page at url "/"
+# display home page at url "/"
 @app.route("/")
 def home():
-    return render_template("app.html")
+    return render_template("index.html")
+
+
+# marvel character page
+@app.route("/marvel/")
+def marvel():
+    return render_template("marvel.html")
+
+
+# DC character page
+@app.route("/dc/")
+def dc():
+    return render_template("dc.html")
